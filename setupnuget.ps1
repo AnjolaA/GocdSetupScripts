dcb45c03ac155a8d76b59e8e16d569cff83b5669
AnjolaA <anjola.awofisoye@sky.uk>
2016-09-02 11:49:23 +0100

    Initial commit

---
+++ setupnuget.ps1
@@ -0,0 +1,15 @@
+﻿If (Test-Path C:\Tools){
+cd C:\Tools
+echo Welcome
+}Else{
+cd C:\
+mkdir Tools
+echo Folder Created
+cd Tools
+}
+Remove-Item "C:\Tools\*"
+$storageDir = $pwd
+$webclient = New-Object System.Net.WebClient
+$url = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+$file = "$storageDir\nuget.exe"
+$webclient.DownloadFile($url,$file)
