f3a0f69a93cabe4257c657c0df4ab868e47403a0
AnjolaA <anjola.awofisoye@sky.uk>
2016-09-02 16:16:50 +0100

    setup modified

--- setupnuget.ps1
+++ setupnuget.ps1
@@ -2,14 +2,15 @@
 
 If (Test-Path C:\Tools){
 cd C:\Tools
-echo Welcome
+echo "Starting Setup"
 }Else{
 cd C:\
 mkdir Tools
+echo "Starting Setup"
 echo Folder Created
 cd Tools
 }
-Remove-Item "C:\Tools\*"
+Remove-Item "C:\Tools\nuget.exe" 
 
 # Setup Nuget
 
@@ -20,8 +21,10 @@ $file = "$storageDir\nuget.exe"
 $webclient.DownloadFile($url,$file)
 
 # Setup NUnit
-
-
+if(Test-Path C:\Tools\bin){
+#do nothing
+}
+else{
 $nunit_url = "https://github.com/nunit/nunit/releases/download/3.4.1/NUnit-3.4.1.zip"
 $nunit_file = "$storageDir\NUnit-3.4.1.zip"
 $webclient.DownloadFile($nunit_url,$nunit_file)
@@ -32,4 +35,9 @@ Add-Type -assembly “system.io.compression.filesystem”
 [io.compression.zipfile]::ExtractToDirectory($nunit_file, $storageDir)
 
 Remove-Item $nunit_file
-echo Nuget and Nunit Setup complete
+
+}
+
+
+
+echo "Nuget and Nunit Setup complete"
